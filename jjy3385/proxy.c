9d39d28f47e68351306acbbb09ff421959a85a52
jjy3385 <jjy3386@gmail.com>
2025-09-03 14:31:36 +0900

    순차처리 프록시 완료 - 1차

--- jjy3385/proxy.c
+++ jjy3385/proxy.c
@@ -1,22 +1,16 @@
 #include "csapp.h"
 
-/* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
+#define DELIM_CHARS "/:"
 
-void doit(int fd);
-void read_requesthdrs(rio_t *rp);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
-void parse_uri(char *uri, char *filename, char *cgiargs);
-
-/* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
 int main(int argc, char **argv) {
-  int listenfd, connfd;
+
+  int listenfd, connfd, clientfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
@@ -28,58 +22,114 @@ int main(int argc, char **argv) {
 
   listenfd = Open_listenfd(argv[1]);
   while (1) {
+
+    rio_t rio, end_rio;
+    char buf[MAXLINE], method[MAXLINE], url[MAXLINE], version[MAXLINE];
     clientlen = sizeof(clientaddr);
+
+    /* 1.리스닝 */
     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
                 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    printf("connfd = %d\n", connfd);
 
-    // 요청 받기
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char filename[MAXLINE], cgiargs[MAXLINE]; 
-  
-    rio_t rio;
     Rio_readinitb(&rio, connfd);
-    if (!Rio_readlineb(&rio, buf, MAXLINE)) {
-      return;
+    Rio_readlineb(&rio, buf, MAXLINE);
+
+    /* 2.파싱 */
+    // HTTP 요청
+    sscanf(buf, "%s %s %s", method, url, version);
+    if (strcasecmp(method, "GET")) {
+      proxy_error(connfd, method, "501", "Not Implemented",
+                  "Proxy does not implement this method");
     }
-    printf("%s", buf);
 
-    //요청 보내기
-    sscanf(buf, "%s %s %s", method, uri, version);
-    parse_uri(uri, filename, cgiargs);
+    char *next_ptr;
+    strtok_r(url, DELIM_CHARS, &next_ptr);
+    char *end_host = strtok_r(NULL, DELIM_CHARS, &next_ptr);
+    char *end_port = strtok_r(NULL, DELIM_CHARS, &next_ptr);
+    char *uri = strtok_r(NULL, DELIM_CHARS, &next_ptr);
 
-    char *saveptr;
-    char *protocal = strtok_r(uri,":",saveptr);
+    char result_uri[MAXLINE];
+    if (uri == NULL) {
+      result_uri[0] = '/';
+    } else {
+      sprintf(result_uri, "/%s", uri);
+    }
 
+    /* 3.엔드서버로 HTTP 요청 전달 */
+    int clientfd = Open_clientfd(end_host, end_port);
+    if (clientfd < 0) {
+      proxy_error(connfd, "end_server", "403", "Forbidden",
+                  "Proxy couldn't find end server");
+    } else {
+      char send[MAXLINE];
+      sprintf(send, "%s %s %s\r\n", method, result_uri, version);
+      Rio_writen(clientfd, send, strlen(send));
 
+      // 헤더
+      sprintf(send, "Host: %s:%s\r\n", end_host, end_port);
+      Rio_writen(clientfd, send, strlen(send));
+      sprintf(send, "%s", user_agent_hdr);
+      Rio_writen(clientfd, send, strlen(send));
+      sprintf(send, "Connection: close\r\n");
+      Rio_writen(clientfd, send, strlen(send));
+      sprintf(send, "Proxy-Connection: close\r\n\r\n");
+      Rio_writen(clientfd, send, strlen(send));
 
-    // rio_t end_rio;
-    // char end_buf[MAXLINE];
-    // int clientfd = Open_clientfd(end_hostname,end_port);
-    // Rio_readinitb(&end_rio, clientfd);
-    // Rio_writen(clientfd, rio.rio_buf,rio.rio_cnt);
-    // Rio_readn(clientfd,end_buf,MAXLINE);
+      /* 4. 응답처리 */
+      Rio_readinitb(&end_rio, clientfd);
+      char recv_header[MAXLINE], recv_body[MAXLINE];
+      int content_length = -1;
 
+      // 헤더응답
+      while (1) {
+        ssize_t n = Rio_readlineb(&end_rio, recv_header, MAXLINE);
+        // 읽기 에러 시 탈출
+        if (n <= 0) {
+          break;
+        }
+        Rio_writen(connfd, recv_header, n);
 
-    // Fputs(end_buf,stdout);
+        // 헤더 끝나면 탈출
+        if (!strcmp(recv_header, "\r\n")) {
+          break;
+        }
 
+        // Content-Length
+        if (!strncasecmp(recv_header, "Content-length",
+                         strlen("Content-length"))) {
+          sscanf(recv_header, "Content-length: %d\r\n", &content_length);
+        }
+      }
 
+      // 바디응답
+      long remain = content_length;
+      while (remain > 0) {
+        ssize_t read = (remain < (long)sizeof(recv_body))
+                           ? (ssize_t)remain
+                           : (ssize_t)sizeof(recv_body);
+        ssize_t n = Rio_readnb(&end_rio, recv_body, read);
+        if (n <= 0) {
+          break;
+        }
+        Rio_writen(connfd, recv_body, n);
+        remain -= n;
+      }
 
-    // Close(connfd);  
-    // Close(clientfd);
-
-
+      Close(clientfd);
+    }
+    Close(connfd);
   }
 
   return 0;
 }
 
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
+void proxy_error(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg) {
   char buf[MAXLINE], body[MAXBUF];
 
+  /* Build the HTTP response body */
   sprintf(body, "<html><title>Proxy Error</title>");
   sprintf(body,
           "%s<body bgcolor="
@@ -88,8 +138,9 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
           body);
   sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
   sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
-  sprintf(body, "%s<hr><em>The Proxy</em>\r\n", body);
+  sprintf(body, "%s<hr><em>The Proxy Web server</em>\r\n", body);
 
+  /* Print the HTTP response */
   sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Content-type: text/html\r\n");
@@ -97,27 +148,4 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
   sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
   Rio_writen(fd, buf, strlen(buf));
   Rio_writen(fd, body, strlen(body));
-}
-
-void parse_uri(char *uri, char *filename, char *cgiargs) {
-  char *ptr;
-
-  if (!strstr(uri, "cgi-bin")) { /* Static content */
-    strcpy(cgiargs, "");
-    strcpy(filename, ".");
-    strcat(filename, uri);
-    if (uri[strlen(uri) - 1] == '/')
-      strcat(filename, "home.html");
-    return;
-  } else { /* Dynamic content */
-    ptr = index(uri, '?');
-    if (ptr) {
-      strcpy(cgiargs, ptr + 1);
-      *ptr = '\0';
-    } else
-      strcpy(cgiargs, "");
-    strcpy(filename, ".");
-    strcat(filename, uri);
-    return;
-  }
 }
